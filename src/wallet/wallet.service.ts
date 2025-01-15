1d627e59f051ce8deca351470c0d2373c0e394b6
mohitM0 <mohit@m0.ventures>
2025-01-15 09:24:51 +0000

    refactor: improve code formatting and error handling in WalletService

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { PrivyClient, User, WalletApiCreateRequestType, WalletApiCreateResponseType } from '@privy-io/server-auth';
+import {
+  PrivyClient,
+  User,
+  WalletApiCreateRequestType,
+  WalletApiCreateResponseType,
+} from '@privy-io/server-auth';
 import AuthTokenService from 'src/_common/service/authToken.service';
 const { v4: uuidv4 } = require('uuid');
 
 @Injectable()
 export class WalletService {
   private readonly privy: PrivyClient;
-  constructor(
-    private authTokenService: AuthTokenService,
-  ) {
+  constructor(private authTokenService: AuthTokenService) {
     const appId = process.env.PRIVY_APP_ID;
     const appSecret = process.env.PRIVY_APP_SECRET;
 
@@ -21,34 +24,40 @@ export class WalletService {
     this.privy = new PrivyClient(appId, appSecret);
   }
 
-  async createServerWallet(id: string, authToken: string): Promise<WalletApiCreateResponseType[]> {
+  async createServerWallet(
+    id: string,
+    authToken: string,
+  ): Promise<WalletApiCreateResponseType[]> {
     const idempotencyKey_ETH = uuidv4();
     const idempotencyKey_SOL = uuidv4();
 
-    console.log("idempotency key:" + idempotencyKey_ETH);
-    console.log("idempotency key:" + idempotencyKey_SOL);
-
+    console.log('idempotency key:' + idempotencyKey_ETH);
+    console.log('idempotency key:' + idempotencyKey_SOL);
 
     const verifiedAuthToken =
       await this.authTokenService.verifyAuthToken(authToken);
     if (!verifiedAuthToken) {
       throw new Error('User is not verified.');
     }
-    console.log('Inside creating server wallet')
-  
+    console.log('Inside creating server wallet');
+
+    const cleanId = id.split('did:privy:')[1];
+    if (!cleanId) {
+      throw new Error('Invalid ID format. Expected "did:privy:<id>".');
+    }
+
     const wallet_ETH = await this.privy.walletApi.create({
       chainType: 'ethereum',
-      authorizationKeyIds: [id],
-      idempotencyKey:idempotencyKey_ETH
+      authorizationKeyIds: [cleanId],
+      idempotencyKey: idempotencyKey_ETH,
     });
-
 
     const wallet_SOL = await this.privy.walletApi.create({
       chainType: 'solana',
-      authorizationKeyIds: [id],
-      idempotencyKey:idempotencyKey_SOL
-    })
-    
+      authorizationKeyIds: [cleanId],
+      idempotencyKey: idempotencyKey_SOL,
+    });
+
     return [wallet_ETH, wallet_SOL];
   }
 }
