840ec59b40290c7c3f3efbb630e2e3340bab6db7
ameyah74 <ameya@m0.ventures>
2025-01-29 08:08:41 +0000

    refactor: remove wallet module and related components to streamline application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,12 @@ import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EvmTxModule } from './evm-tx/evm-tx.module';
-import { WalletModule } from './wallet/wallet.module';
 import { AccessTokenMiddleware } from './_common/middleware/accessToken.middleware';
-import { WalletController } from './wallet/wallet.controller';
 import { EvmTxController } from './evm-tx/evm-tx.controller';
 
 @Module({
   imports: [
     EvmTxModule, 
-    WalletModule,
     RedisModule.forRoot({
       config: {
         host: process.env.REDIS_HOST,
@@ -28,6 +25,6 @@ export class AppModule implements NestModule{
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(AccessTokenMiddleware)
-      .forRoutes(WalletController, EvmTxController);
+      .forRoutes(EvmTxController);
   }
 }
--- src/wallet/dto/create-server-wallet.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsInt, IsEnum, IsNotEmpty } from '@nestjs/class-validator';
-
-
-export class CreateServerWalletDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly id: string;
-}
