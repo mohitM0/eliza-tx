56582b5e587a0f5fa3ee63bcf41be9c13800a4ec
ameyah74 <ameya@m0.ventures>
2025-02-03 06:38:33 +0000

    feat: add PrismaService to EvmTxModule providers

--- src/evm-tx/evm-tx.module.ts
+++ src/evm-tx/evm-tx.module.ts
@@ -3,9 +3,10 @@ import { EvmTxService } from './evm-tx.service';
 import { EvmTxController } from './evm-tx.controller';
 import AuthTokenService from 'src/_common/service/authToken.service';
 import WalletClientService from 'src/_common/service/walletClient.service';
+import { PrismaService } from 'src/_common/service/prisma.service';
 
 @Module({
   controllers: [EvmTxController],
-  providers: [EvmTxService, AuthTokenService, WalletClientService],
+  providers: [EvmTxService, AuthTokenService, WalletClientService, PrismaService],
 })
 export class EvmTxModule {}
